914d5bd305ee6d3d8c7414e4c0884bbf0171ec55
ipatate <faramazpat@gmail.com>
2022-09-01 21:39:03 +0200

    add patterns wrapper

---
+++ patterns/wrapper.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Title: Wrapper
+ * Slug: goodmotions/wrapper
+ * Categories: mercredi/mercredi-patterns
+ */
+?>
+
+<!-- wp:group {"className":"gm-wrapper"} -->
+<div class="wp-block-group gm-wrapper">
+  <!-- wp:group  -->
+  <div class="wp-block-group">
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
