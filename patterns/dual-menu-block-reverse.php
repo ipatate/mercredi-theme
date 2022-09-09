b25f85fd3b873be40e40cd1c603404538192f2d6
ipatate <faramazpat@gmail.com>
2022-09-09 14:27:44 +0200

    add dual menu

---
+++ patterns/dual-menu-block-reverse.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Title: Dual Menu block Reverse
+ * Slug: goodmotions/dual-menu-block-reverse
+ * Categories: mercredi/mercredi-patterns
+ */
+?>
+
+<!-- wp:group {"align":"full","className":"gm-dual-menu-block gm-block-reverse"} -->
+<div class="wp-block-group alignfull gm-dual-menu-block gm-block-reverse">
+  <!-- wp:group {"backgroundColor":"accent-clear"} -->
+  <div class="wp-block-group has-accent-clear-background-color has-background">
+    <!-- wp:group {"backgroundColor":"background"} -->
+    <div class="wp-block-group has-background-background-color has-background">
+      <!-- wp:heading {"textAlign":"center","textColor":"accent"} -->
+      <h2 class="has-text-align-center has-accent-color has-text-color">Le menu de la semaine</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"align":"center","textColor":"accent","fontSize":"large"} -->
+      <p class="has-text-align-center has-accent-color has-text-color has-large-font-size">du Mercredi 25 mai au Samedi 28 mai</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"align":"center"} -->
+      <p class="has-text-align-center">Taboulé aux agrumes – orange et pamplemousse - tomates confites, oignons doux, concombre et jambon sec.<br><br>Crème dessert au cacao,<br>éclats de cookie et fondant au chocolat</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"align":"center","textColor":"accent"} -->
+      <p class="has-text-align-center has-accent-color has-text-color">Notre plat du jour et le crumble de légumes sont également disponibles à la vente à emporter.</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:image {"id":1764,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="http://mercredi.localhost/wp-content/uploads/2022/01/briochecremeamandefleurdoranger-e1641898206454-1006x1024.jpg" alt="Brioche crème amande fleur do'ranger Mercredi salon de thé lyon 2 gouter brunch restaurant" class="wp-image-1764" />
+      <figcaption>Brioche crème amande fleur do'ranger Mercredi salon de thé lyon 2 gouter brunch restaurant</figcaption>
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
---
+++ patterns/dual-menu-block.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Title: Dual Menu block
+ * Slug: goodmotions/dual-menu-block
+ * Categories: mercredi/mercredi-patterns
+ */
+?>
+
+<!-- wp:group {"align":"full","className":"gm-dual-menu-block"} -->
+<div class="wp-block-group alignfull gm-dual-menu-block">
+  <!-- wp:group {"backgroundColor":"accent-clear"} -->
+  <div class="wp-block-group has-accent-clear-background-color has-background">
+    <!-- wp:group {"backgroundColor":"background"} -->
+    <div class="wp-block-group has-background-background-color has-background">
+      <!-- wp:heading {"textColor":"accent"} -->
+      <h2 class="has-accent-color has-text-color">Nos menus</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"textColor":"accent","fontSize":"large"} -->
+      <p class="has-accent-color has-text-color has-large-font-size">Envie de salé ou de sucré ? Découvrez nos cartes ci-dessous...</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+      <div class="wp-block-buttons">
+        <!-- wp:button {"backgroundColor":"background","textColor":"accent"} -->
+        <div class="wp-block-button"><a class="wp-block-button__link has-accent-color has-background-background-color has-text-color has-background">Les Goûter et Boissons</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"background","textColor":"accent"} -->
+        <div class="wp-block-button"><a class="wp-block-button__link has-accent-color has-background-background-color has-text-color has-background">Les Déjeuners</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"background","textColor":"accent"} -->
+        <div class="wp-block-button"><a class="wp-block-button__link has-accent-color has-background-background-color has-text-color has-background">Le Brunch</a></div>
+        <!-- /wp:button -->
+      </div>
+      <!-- /wp:buttons -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:image {"id":1764,"sizeSlug":"large","linkDestination":"none"} -->
+    <figure class="wp-block-image size-large"><img src="http://mercredi.localhost/wp-content/uploads/2022/01/briochecremeamandefleurdoranger-e1641898206454-1006x1024.jpg" alt="Brioche crème amande fleur do'ranger Mercredi salon de thé lyon 2 gouter brunch restaurant" class="wp-image-1764" />
+      <figcaption>Brioche crème amande fleur do'ranger Mercredi salon de thé lyon 2 gouter brunch restaurant</figcaption>
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
